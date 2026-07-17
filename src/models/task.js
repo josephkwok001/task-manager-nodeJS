c0f4cb2ca7d5ecc965eb3006bc7d89342831faf2
Joseph <177612090+josephkwok001@users.noreply.github.com>
2026-07-17 21:10:24 +0800

    Rest API for task and users

---
+++ src/models/task.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+const validator = require("validator")
+
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api")
+
+const Task = mongoose.model("Task", {
+    description: {
+        type: String,
+        trim: true,
+        required: true
+    }, 
+    isComplete: {
+        type: Boolean,
+        default: false
+    }
+})
+
+
+module.exports = Task
