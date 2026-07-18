67a2ce8b43dcd8baf861f7983415844e267b4fdd
Joseph <177612090+josephkwok001@users.noreply.github.com>
2026-07-18 17:42:06 +0800

    Create log in authentacation

--- playground/promise-chaining2.js
+++ playground/promise-chaining2.js
@@ -4,11 +4,14 @@ const Task = require("../src/models/task")
 
 
 
-Task.findByIdAndDelete("6a5a24e4be24c8983a2d1207").then((task) => {
-    console.log(task)
-    return Task.countDocuments({ isComplete : false})
-}).then(result => {
-    console.log(result)
-}).catch((e) => { 
-    console.log(e)
-})
+// Task.findByIdAndDelete("6a5a24e4be24c8983a2d1207").then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ isComplete : false})
+// }).then(result => {
+//     console.log(result)
+// }).catch((e) => { 
+//     console.log(e)
+// })
+
+
+
