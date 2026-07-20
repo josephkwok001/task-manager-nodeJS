a0dc77e0becdbb4aa9ac3bc89c3c8f102152242c
Joseph <177612090+josephkwok001@users.noreply.github.com>
2026-07-20 16:55:57 +0800

    Logout feature

--- src/routers/user.js
+++ src/routers/user.js
@@ -41,6 +41,29 @@ router.post("/users/login", async (req, res) => {
 
 })
 
+router.post("/users/logout", auth, async (req, res) => {
+    try {
+        // filters the only device's token from the tokens array (other device stays logged in)
+        req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token)
+
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.post("/users/logoutAll", auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 router.get("/users/me", auth, async (req, res) => {
     res.send(req.user)
 })
