8010bfc7d914941e2d88eb18274ad613ceb5e3b4
Joseph <177612090+josephkwok001@users.noreply.github.com>
2026-07-21 18:02:44 +0800

    Validating file uploads

--- src/index.js
+++ src/index.js
@@ -7,6 +7,28 @@ const app = express()
 const port = process.env.PORT || 3000
  
 
+const multer = require("multer")
+
+const upload = multer({
+    dest: "images",
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error("please upload a Word Doc"))
+        }
+
+        cb(undefined, true)
+    }
+})
+
+app.post("/upload", upload.single("upload"), (req, res) => {
+    res.send()
+})
+
+
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
